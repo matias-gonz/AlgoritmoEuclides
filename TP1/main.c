4221121e3587f151db1e100e0d688a91bd0ea430
Matías Ignacio González <74241889+matias-gonz@users.noreply.github.com>
2021-05-31 04:07:44 +0300

    Update main.c

--- TP1/main.c
+++ TP1/main.c
@@ -1,6 +1,68 @@
 #include "args_parser.h"
 #include "gcd.h"
 
+
+int parse_line(char* line, int* a, int* b){
+    char *found;
+
+    found = strtok(line," ");
+    if( !found)
+        return 1;
+
+    *a = atoi(found);
+
+    found = strtok(NULL," ");
+    if(!found)
+        return 1;
+
+    *b = atoi(found);
+
+    found = strtok(NULL," ");
+    if(found)
+        return 1;
+
+    return 0;
+}
+
+struct gcd *read_file(FILE* file, size_t *len){
+    char line[100];
+    struct gcd *gcd = NULL;
+    int a, b;
+    int line_read = fscanf(file, "%99[^\n]\n", line);
+    if(line_read == EOF){
+        perror("Empty file");
+        return NULL;
+    }
+    int parse_line_success = parse_line(line,&a,&b);
+    if(parse_line_success == 1)
+    {
+        perror("Error while parsing line");
+        return NULL;
+    }
+
+    while (line_read != EOF)
+    {
+        gcd = realloc(gcd, ((*len) + 1) * sizeof(struct gcd));
+        if (!gcd)
+        {
+            perror("Error realloc");
+            return NULL;
+        }
+        gcd[*len].num_a = a;
+        gcd[*len].num_b = b;
+        (*len)++;
+        line_read = fscanf(file, "%99[^\n]\n", line);
+        if(line_read != EOF)
+            parse_line_success = parse_line(line,&a,&b);
+        if(parse_line_success == 1)
+        {
+            perror("Error while parsing line");
+            return NULL;
+        }
+    }
+    return gcd;
+}
+
 int main(int argc, char **argv)
 {
     args_t args;
@@ -12,31 +74,30 @@ int main(int argc, char **argv)
 
     if (args.in_file != NULL)
     {
-        int a, b;
-        int read = fscanf(args.in_file, "%i %i\n", &a, &b);
-        while (read == 2)
-        {
-            gcd = realloc(gcd, (len + 1) * sizeof(struct gcd));
-            if (!gcd)
-            {
-                perror("Error realloc");
-            }
-            gcd[len].num_a = a;
-            gcd[len].num_b = b;
-            len++;
-            read = fscanf(args.in_file, "%i %i\n", &a, &b);
+        gcd = read_file(args.in_file,&len);
+        if(!gcd){
+            error = 1;
         }
     }
 
-    euclides(gcd, len);
-
-    if (args.out_file != NULL)
+    if(error != 1)
     {
-        for (size_t i = 0; i < len; i++)
+        euclides(gcd, len);
+
+        if (args.out_file != NULL)
         {
-            fprintf(args.out_file, "GCD(%i,%i) = %i\n", gcd[i].num_a, gcd[i].num_b, gcd[i].gcd_ab);
+            for (size_t i = 0; i < len; i++)
+            {
+                if(fprintf(args.out_file, "GCD(%i,%i) = %i\n", gcd[i].num_a, gcd[i].num_b, gcd[i].gcd_ab) < 0)
+                {
+                    perror("Error at writing file");
+                    error = 1;
+                    i = len;
+                }
+            }
         }
     }
+    
 
     if (args.in_file != NULL)
     {
